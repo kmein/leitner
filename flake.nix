4d38e69c5b649bd031103f61defaa8718085060a
Kierán Meinhardt <kmein@posteo.de>
2024-06-05 08:57:02 +0200

    flake: add rustfmt

--- flake.nix
+++ flake.nix
@@ -6,15 +6,11 @@
   };
 
   outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
     packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
     devShells.x86_64-linux.default = let
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
     in pkgs.mkShell {
-      packages = [ pkgs.cargo ];
+      packages = [ pkgs.cargo pkgs.rustfmt ];
     };
   };
 }
